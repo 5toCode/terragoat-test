67c04a7236aac94b57ac6d44222699d82ba51ae8
5toCode <mike70@gmail.com>
2021-12-13 17:26:24 -0500

    add new Azure service

---
+++ terraform/azure/new.tf
@@ -0,0 +1,10 @@
+resource azurerm_app_service "app-service2" {
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = var.location
+  name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
+  resource_group_name = azurerm_resource_group.example.name
+
+  site_config {
+    dotnet_framework_version = "v5.0"
+  }
+}
